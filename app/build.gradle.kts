a1b0dcece30cc2c4b6072c124b110e82f743b697
kalilpock <kalilpock54@gmail.com>
2025-11-13 13:45:34 -0300

    correcaoBuild

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,10 +66,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    val room_version = "2.6.1"
-    implementation("androidx.room:room-runtime:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-    ksp("androidx.room:room-compiler:$room_version")
+    
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
 
 }

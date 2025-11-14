e1b5c55e000df38c09386db71efe13e58c4ec9e7
Matheus Müller <matheusmullerdosantos@gmail.com>
2025-11-13 22:15:35 -0300

    refactor: AuddModels.kt

--- app/src/main/java/com/example/musiletra/data/RetrofitClient.kt
+++ app/src/main/java/com/example/musiletra/data/RetrofitClient.kt
@@ -1,41 +1,12 @@
 package com.example.musiletra.data
 
 import com.example.musiletra.BuildConfig
+import com.example.musiletra.model.AudDApiService
 import com.google.gson.annotations.SerializedName
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-
-// Data classes to model the JSON response from AudD API
-data class AudDResponse(
-    val status: String,
-    val result: List<AudDSong>?,
-    val error: AudDError? // Adicionado para capturar mensagens de erro
-)
-
-data class AudDSong(
-    val title: String,
-    val artist: String,
-    val lyrics: String
-)
-
-// Classe para o objeto de erro da API
-data class AudDError(
-    @SerializedName("error_message") val message: String,
-    @SerializedName("error_code") val code: Int
-)
-
-
-// Retrofit service interface
-public interface AudDApiService {
-    @GET("/")
-    suspend fun findByLyrics(
-        @Query("q") query: String,
-        @Query("method") method: String = "findLyrics",
-        @Query("api_token") apiToken: String = BuildConfig.AUDD_API_KEY
-    ): AudDResponse
-}
 
 // Retrofit client singleton
 object RetrofitClient {
--- app/src/main/java/com/example/musiletra/model/AuddModels.kt
+++ app/src/main/java/com/example/musiletra/model/AuddModels.kt
@@ -1,4 +1,36 @@
 package com.example.musiletra.model
 
-class AuddModels {
+import com.example.musiletra.BuildConfig
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+// Data classes to model the JSON response from AudD API
+data class AudDResponse(
+    val status: String,
+    val result: List<AudDSong>?,
+    val error: AudDError? // Adicionado para capturar mensagens de erro
+)
+
+data class AudDSong(
+    val title: String,
+    val artist: String,
+    val lyrics: String
+)
+
+// Classe para o objeto de erro da API
+data class AudDError(
+    @SerializedName("error_message") val message: String,
+    @SerializedName("error_code") val code: Int
+)
+
+
+// Retrofit service interface
+public interface AudDApiService {
+    @GET("/")
+    suspend fun findByLyrics(
+        @Query("q") query: String,
+        @Query("method") method: String = "findLyrics",
+        @Query("api_token") apiToken: String = BuildConfig.AUDD_API_KEY
+    ): AudDResponse
 }
